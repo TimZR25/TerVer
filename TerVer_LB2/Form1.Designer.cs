777329a5eb1d85998e7f8261ee85b63c09d9db01
Тимур Зарипов <Timon_Z@inbox.ru>
2023-05-11 21:11:48 +0700

    Добавьте файлы проекта.

---
+++ TerVer_LB2/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace TerVer_LB2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(449, 38);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(500, 500);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 435);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сгенерировать числа";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(30, 220);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(26, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Выборка";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(235, 435);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 50);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Нарисовать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(345, 83);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(26, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Интервал";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(30, 283);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 8;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(256, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Среднее";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(256, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Мода";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(256, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Медиана";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(256, 325);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(188, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Выборочная дисперсия";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(260, 184);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(260, 240);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(260, 293);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(260, 348);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 606);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
---
+++ TerVer_LB2/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TerVer_LB2
+{
+    public partial class Form1 : Form
+    {
+        private int Selection; // выборка
+        private List<double> Numbers; // СВ
+        private int Interval; // интервал
+
+        private double _average;
+        public double Average   // среднее
+        {
+            get => _average;
+            set
+            {
+                _average = Math.Round(value, 2);
+                textBox2.Text = _average.ToString();
+            }
+        }
+
+        private double _mode;
+        public double Mode  // мода
+        {
+            get => _mode;
+            set
+            {
+                _mode = Math.Round(value, 2);
+                textBox3.Text = _mode.ToString();
+            }
+        }
+
+        private double _median;
+        public double Median    // медиана
+        {
+            get => _median;
+            set
+            {
+                _median = Math.Round(value, 2);
+                textBox4.Text = _median.ToString();
+            }
+        }
+
+        private double _sampleVariance;
+        public double SampleVariance    //выборочная дисперсия
+        {
+            get => _sampleVariance;
+            set
+            {
+                _sampleVariance = Math.Round(value, 2);
+                textBox5.Text = _sampleVariance.ToString();
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Numbers = new List<double>();
+            numericUpDown1.Maximum = 10000;
+            numericUpDown1.Minimum = 100;
+            numericUpDown2.Maximum = numericUpDown1.Minimum;
+            numericUpDown2.Minimum = 5;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GenerateNumbers();
+            CalculateParameters();
+        }
+
+        private void GenerateNumbers()  // Сгенерировать СВ методом усечения
+        {
+            numericUpDown2.Maximum = numericUpDown1.Value;
+            Numbers.Clear();
+            textBox1.Clear();
+
+            Random random = new Random();
+            double x, y;
+            for (int i = 0; i < Selection; i++) // генерация СВ методом усечения
+            {
+                do
+                {
+                    y = random.NextDouble();
+                    x = random.NextDouble() * 2 + 1;
+                } while (y > (1.5 - 0.5 * x));
+                x = Math.Round(x, 2);
+                Numbers.Add(x);
+                textBox1.Text += "(" + x.ToString() + ") ";
+            }
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            Selection = (int)numericUpDown1.Value;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            Interval = (int)numericUpDown2.Value;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Numbers.Count == 0) return;
+
+            DrawHistogram();
+        }
+
+        private void DrawHistogram() // Нарисовать гистограмму 
+        {
+            chart1.Series[0].Points.Clear();
+
+            Numbers.Sort();
+
+            int numsInColumn = Numbers.Count / Interval;
+            for (int i = 0; i < Interval; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < numsInColumn; j++)
+                {
+                    sum += Numbers[j + i * numsInColumn];
+                }
+                double x = sum / numsInColumn;
+                double y = 1.5 - 0.5 * x;
+                x = Math.Round(x, 3);
+                chart1.Series[0].Points.AddXY(x, y);
+            }
+        }
+
+        private void CalculateParameters() // Находит параметры СВ
+        {
+            Average = Numbers.Sum() / Numbers.Count; // среднее
+
+
+            Median = Numbers.Count % 2 == 0 ?
+                (Numbers[Numbers.Count / 2] + Numbers[Numbers.Count / 2 - 1]) / 2
+                : Numbers[Numbers.Count / 2]; // медиана
+
+
+            List<double> numbers = Numbers;
+            numbers.Sort();
+            double num = numbers[0];
+            double maxNum = num;
+            int maxCount = 0;
+            int count = 0;
+            for (int i = 0; i < numbers.Count; i += count + 1)
+            {
+                count = 0;
+                num = numbers[i];
+                for (int j = i; j < numbers.Count && num == numbers[j]; j++)
+                    count++;
+
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    maxNum = num;
+                }
+            }
+            Mode = maxNum; // мода
+
+
+            double sum = 0;
+            for (int i = 0; i < Numbers.Count; i++)
+            {
+                sum += Math.Pow(Numbers[i] - Average, 2);
+            }
+            SampleVariance = sum / Numbers.Count; //выборочная дисперсия
+
+        }
+    }
+}
